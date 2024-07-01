75f41f18ada8ae75b108d42d713650c92b8c65c2
Bhakta <Bhaktathapamgr5@gmail.com>
2024-07-01 17:57:13 +0000

    Updated app.py

--- vote/app.py
+++ vote/app.py
@@ -7,7 +7,7 @@ import json
 import logging
 
 option_a = os.getenv('OPTION_A', "DevOps")
-option_b = os.getenv('OPTION_B', "Developer")
+option_b = os.getenv('OPTION_B', "Bhakta")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
